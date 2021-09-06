e2c90a7fe39137beb4a8d458612864e1fae4bff3
cepa995 <stefan.ai.research@gmail.com>
2021-09-07 00:29:22 +0200

    Adding torchio dataset class and torchio based visualization class

--- toolbox/visualization.py
+++ toolbox/visualization.py
@@ -1,6 +1,9 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import torch
+import torchio as tio
+
 from PyPDF2 import PdfFileMerger
 from matplotlib.backends.backend_pdf import PdfPages
 from toolbox.logger import logging
@@ -8,6 +11,96 @@ from toolbox.dicom import DicomReader
 
 train_image_root = '/kaggle/input/rsna-miccai-brain-tumor-radiogenomic-classification/train'
 test_image_root = '/kaggle/input/rsna-miccai-brain-tumor-radiogenomic-classification/test'
+
+
+class VisualizeMedicalDataset():
+    def __init__(self, data: tio.SubjectsDataset,  dataset_type: str):
+        """ VisaulizeMedicalDataset constructor
+
+        :param data: torchio.SubjectsDataset which we want to visualize.
+        :param dataset_type: dataset type (i.e., training, validation)
+        """
+        self.data = data
+        self.dataset_type = dataset_type
+        # self.max_value = self.get_max_value()
+        # self.min_value = self.get_min_value()
+
+    @staticmethod
+    def plot_histogram(axis, tensor, num_positions=100, label=None, alpha=0.05, color=None):
+        """ Plots Histogram.
+
+        :param axis: matplotlib axis
+        :param num_positions: number of positions
+        :param label: plot label
+        :param alpha: opacity
+        :param color: color of the plot
+        """
+        values = tensor.numpy().ravel()
+        kernel = stats.gaussian_kde(values)
+        positions = np.linspace(values.min(), values.max(), num=num_positions)
+        histogram = kernel(positions)
+        kwargs = dict(linewidth=1, color='black' if color is None else color, alpha=alpha)
+        if label is not None:
+            kwargs['label'] = label
+        axis.plot(positions, histogram, **kwargs)
+
+    def compute_histograms(self):
+        """ Performs Histogram Standardization of intensity values.
+        """
+        fix, ax = plt.subplots(dpi=100)
+        for subject in tqdm_notebook(self.data.dry_iter()):
+            tensor = subject.get_first_image().data
+            if T1 in subject.__dict__.keys():
+                color = 'red'
+            elif T1GD in subject.__dict__.keys():
+                color = 'yellow'
+            elif T2 in subject.__dict__.keys():
+                color = 'green'
+            elif FLAIR in subject.__dict__.keys():
+                color = 'blue'
+            plot_histogram(ax, tensor, color=color)
+        # ax.set_xlim(self.min_value, self.max_value)
+        ax.set_xlim(-100, 5000)
+        ax.set_ylim(0, 0.004)
+        ax.set_title(f"Intensity Histogram for {self.dataset_type} Dataset")
+        ax.set_xlabel("Intensity Value")
+        ax.grid()
+
+    def plot_subject(self, subject_id=None):
+        """ Plots subject that corresponds to the specified subject ID, or
+        random if not specfiied..
+
+        :param subject_id - string by which subjects are uniquely identified.
+        """
+        if subject_id:
+            subject_idx = [idx for idx, subject in enumerate(self.data.dry_iter(), 0)
+                           if subject.__dict__['subject_id'] == subject_id][0]
+            if not subject_idx:
+                raise ValueError(f"[ERROR]: Subject with {subject_id} does not exist in the dataset!")
+        else:
+            subject_ids = [subject.__dict__['subject_id'] for subject in self.data.dry_iter()]
+            subject_idx = random.randint(0, len(subject_ids))
+        self.data[subject_idx].plot()
+
+    def get_max_value(self):
+        """ Returns maximum pixel value based on list of subjects
+
+        :param subjects - list of subjects for which we want
+         to retrieve maximum value
+        :retunr maximum subject value
+        """
+        values = np.array([image.numpy().max() for s in self.data for image in s.get_images()])
+        return values.max(axis=0)
+
+    def get_min_value(self):
+        """ Returns maximum pixel value based on list of subjects
+
+        :param subjects - list of subjects for which we want
+         to retrieve maximum value
+        :retunr maximum subject value
+        """
+        values = np.array([image.numpy().min() for s in self.data for image in s.get_images()])
+        return values.min(axis=0)
 
 
 def show_n_slices(array, start_from=0, step=10, columns=6, figsize=(18,10)):
