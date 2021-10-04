14efed2b14fa4e3269aede52a3273f499c673c87
pohan <l.w.jasons@gmail.com>
2021-10-04 09:01:37 -0400

    change the landmarks location

--- toolbox/constants.py
+++ toolbox/constants.py
@@ -6,4 +6,4 @@ T1GD = "T1wCE"
 FLAIR = "FLAIR"
 ALL_SEQUENCES = [T1, T2, T1GD, FLAIR]
 
-INPUT_DIR = "C:\\Users\\StefanCepa995\\Desktop\\Machine Learning\\Kaggle Competitions\\rsna-miccai-brain-tumor-radiogenomic-classification\\rsna-miccai-brain-tumor-radiogenomic-classification"
+INPUT_DIR = "/kaggle/input"
