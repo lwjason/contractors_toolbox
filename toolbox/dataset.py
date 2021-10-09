f5e2bede930eb5e5d3894c15c9ee879ffa80aa9f
Pohan Chen <Jason.Chen@Bioclinica.com>
2021-10-09 11:05:55 +0800

    Make patch_size configurable

--- toolbox/dataset.py
+++ toolbox/dataset.py
@@ -32,7 +32,7 @@ class RSNA_MICCAIBrainTumorDataset(pl.LightningDataModule):
     """
 
     def __init__(self, sequence: str, dataset_dir, batch_size, train_label_csv, train_val_ratio, task=None, preprocess=None,
-                 augmentation=None, num_workers=0, patch_based_mode=False, sample_per_volume=10):
+                 augmentation=None, num_workers=0, patch_based_mode=False, sample_per_volume=10, patch_size=96):
 
         super().__init__()
         self.check_sequence_valid(sequence)
@@ -52,6 +52,7 @@ class RSNA_MICCAIBrainTumorDataset(pl.LightningDataModule):
         self.test_set = None
         self.patch_based_mode = patch_based_mode
         self.sample_per_volume = sample_per_volume
+        self.patch_size = patch_size
 
     @staticmethod
     def check_sequence_valid(sequence):
@@ -226,7 +227,7 @@ class RSNA_MICCAIBrainTumorDataset(pl.LightningDataModule):
         return train_dict, test_dict
 
     def create_queue(self, dataset):
-        patch_size = 96
+        patch_size = self.patch_size
         queue_length = 300
         samples_per_volume = self.sample_per_volume
         sampler = tio.data.UniformSampler(patch_size=patch_size)
